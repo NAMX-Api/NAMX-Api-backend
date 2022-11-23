4c3e4337b35bfa5fee33f757a5fc01620d84d046
Ayoub Basidi <93930160+AYOUBBASIDI@users.noreply.github.com>
2022-11-23 09:12:14 +0100

    delete oldest version api

--- API/public/Controllers/hello.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-
-exports.test = async function (req, res) {
-    res.json("hello body");
-}
--- API/public/routes/route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Hello = require('../Controllers/hello.js');
-
-router.get('/hello', Hello.test);
-
-module.exports = router;
