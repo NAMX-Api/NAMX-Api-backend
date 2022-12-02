6d114c475a9a6e681683a8f882af1efd375c5e1f
oussama <oussama.ennadafy@gmail.com>
2022-12-02 09:29:16 +0100

    nothing to commit

---
+++ api/controllers/usersController.js
@@ -0,0 +1,22 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+
+const getAllUsers = (req, res) => {
+    res.json(usersDB.users);
+}
+
+const getUser = (req, res) => {
+    const user = usersDB.users.find(userData => userData.id === parseInt(req.params.id));
+    if (!user) {
+        return res.status(400).json({ "message": `user ID ${req.params.id} not found` });
+    }
+    res.json(user);
+}
+
+
+module.exports = {
+    getAllUsers,
+    getUser
+}
